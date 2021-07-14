16bf7fa6ae645fa8c541fae33b0a2adf370fb0ff
BuildTools <sarquentin@gmail.com>
2021-07-14 17:08:07 +0200

    Modify animation speed

--- public/js/round.js
+++ public/js/round.js
@@ -12,7 +12,7 @@ document.addEventListener("scroll", () => {
         if (scrollPos >= (part5.getBoundingClientRect().top + window.scrollY - 250))
         {
             Array.from(circles).forEach(circle => {
-                circle.style.strokeDasharray = progress * 4.65 + " 999";
+                circle.style.strokeDasharray = progress * 4.65 + ' 999';
             });
             active = true
         }
