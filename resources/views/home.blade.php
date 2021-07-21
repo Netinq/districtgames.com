4d8887cfaea93ed243b36bf2b82b64a2bd2ac952
BuildTools <sarquentin@gmail.com>
2021-07-22 01:26:07 +0200

    Adding price section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
 @include('home.section-5')
 @include('home.section-6')
 @include('home.section-7')
+@include('home.section-8')
 <script src="{{ asset('js/haut.js')}}"></script>
 <script src="{{ asset('js/carousel.js')}}"></script>
 <script src="{{ asset('js/scroll.js')}}"></script>
---
+++ resources/views/home/section-8.blade.php
@@ -0,0 +1,18 @@
+<section id="part-8">
+    <div class="content">
+        <div id="price" class="box">
+            <img src="{{ asset('img/home/part-8/price.png')}}" alt="Price's board" class="board">
+            <div class="txt">
+                <h3>Fiche <span>tarifaire</span></h3>
+                <p>Téléchargez le .pdf pour visualiser nos fiches tarifaires.</p>
+                <a href="#" download=""><img src="{{ asset('img/home/part-8/download_districtgames.png')}}" alt="Download button"></a>
+                <a href="#" download=""><img src="{{ asset('img/home/part-8/download_districtlight.png')}}" alt="Download button"></a>
+            </div>
+        </div>
+        <div id="joinus" class="box">
+            <a href="#"><img src="{{ asset('img/home/part-8/twitter.png')}}" alt="Twitter link"></a>
+            <a href="#"><img src="{{ asset('img/home/part-8/discord.png')}}" alt="Discord link"></a>
+            <a href="#"><img src="{{ asset('img/home/part-8/instagram.png')}}" alt="Instagram link"></a>
+        </div>
+    </div>
+</section>
