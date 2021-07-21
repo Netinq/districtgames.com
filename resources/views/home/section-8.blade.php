7317d930f9aa3e06ce779cd0cebf6242a82c5d62
BuildTools <sarquentin@gmail.com>
2021-07-22 01:28:53 +0200

    Adding links

--- resources/views/home/section-8.blade.php
+++ resources/views/home/section-8.blade.php
@@ -5,14 +5,14 @@
             <div class="txt">
                 <h3>Fiche <span>tarifaire</span></h3>
                 <p>Téléchargez le .pdf pour visualiser nos fiches tarifaires.</p>
-                <a href="#" download=""><img src="{{ asset('img/home/part-8/download_districtgames.png')}}" alt="Download button"></a>
-                <a href="#" download=""><img src="{{ asset('img/home/part-8/download_districtlight.png')}}" alt="Download button"></a>
+                <a href="{{ asset('upload/FichePrix_DistricGames.pdf')}}" download=""><img src="{{ asset('img/home/part-8/download_districtgames.png')}}" alt="Download button"></a>
+                <a href="{{ asset('upload/FichePrix_DistricGames.pdf')}}" download=""><img src="{{ asset('img/home/part-8/download_districtlight.png')}}" alt="Download button"></a>
             </div>
         </div>
         <div id="joinus" class="box">
-            <a href="#"><img src="{{ asset('img/home/part-8/twitter.png')}}" alt="Twitter link"></a>
-            <a href="#"><img src="{{ asset('img/home/part-8/discord.png')}}" alt="Discord link"></a>
-            <a href="#"><img src="{{ asset('img/home/part-8/instagram.png')}}" alt="Instagram link"></a>
+            <a href="https://twitter.com/District_Games" target="_blank"><img src="{{ asset('img/home/part-8/twitter.png')}}" alt="Twitter link"></a>
+            <a href="#" target="_blank"><img src="{{ asset('img/home/part-8/discord.png')}}" alt="Discord link"></a>
+            <a href="https://www.instagram.com/districtgames.off/" target="_blank"><img src="{{ asset('img/home/part-8/instagram.png')}}" alt="Instagram link"></a>
         </div>
     </div>
 </section>
