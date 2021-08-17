37e8302c7d29772148114900069aaaede6c00405
BuildTools <sarquentin@gmail.com>
2021-08-17 02:02:11 +0200

    Adding mobile logo

--- resources/views/home/section-1.blade.php
+++ resources/views/home/section-1.blade.php
@@ -1,5 +1,6 @@
 <section id="part-1">
     <div class="flex">
+        <img id="logo-m" src="{{ asset('img/logo-fluid.png')}}" alt="Logo">
         <img id="people" src="{{ asset('img/home/part-1/dessin.png') }}" alt="People draw">
         <img id="cloud" class="cloud" src="{{ asset('img/home/part-1/cloud.svg') }}" alt="People draw">
         <img id="cloud2" class="cloud" src="{{ asset('img/home/part-1/cloud.svg') }}" alt="People draw">
