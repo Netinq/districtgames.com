b421bf25c6e18e65da354fd729e4589897ca5b8c
BuildTools <sarquentin@gmail.com>
2021-08-30 01:29:17 +0200

    Adding contact page

--- resources/views/contact/section-1.blade.php
+++ resources/views/contact/section-1.blade.php
@@ -1,3 +1,16 @@
 <section id="part-1">
-
+    <div class="title">
+        <h2>nous <span>contacter</span>
+        </h2>
+    </div>
+    <div class="pan">
+        <img class="stick" src="{{asset('img/contact/stick.png')}}" alt="">
+        <a href="https://twitter.com/District_Games" target="_blank"><img src="{{asset('img/contact/twitter.png')}}" alt=""></a>
+        <a href="https://www.instagram.com/districtgames.off/" target="_blank"><img src="{{asset('img/contact/insta.png')}}" alt=""></a>
+        <a href="mailto:districtcorp.private@gmail.com" target="_blank"><img src="{{asset('img/contact/email.png')}}" alt=""></a>
+    </div>
+    <div class="discord">
+        <img src="{{asset('img/contact/discord.png')}}" alt="">
+    </div>
+    <img id="chat" src="{{ asset('img/infos/chat.png')}}" alt="">
 </section>
