5b13b53f128792dd9ee443429ef5b38e38a0bfed
BuildTools <sarquentin@gmail.com>
2021-09-15 15:58:51 +0200

    Kill UX

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,6 +6,13 @@
         <a href="{{ route('shop')}}">@lang('header.shop')</a>
         <a href="{{ route('infos')}}">infos</a>
     </nav>
+    <a id="lang" class="{{ Route::is('portfolio') ? 'blue' : '' }}{{ Route::is('contact') ? 'brown' : '' }}" href="{{ App::getLocale() == 'fr' ? route('lang', 'en') : route('lang', 'fr')}}">
+@if (App::getLocale() != 'fr')
+<img src="{{ asset('img/FR.png')}}" alt="Download button">
+@else
+<img src="{{ asset('img/EN.png')}}" alt="Download button">
+@endif
+</a>
     {{-- <img class="mobile logo" src="{{ asset('img/logo-fluid.png')}}" alt="Logo"> --}}
     <button onclick="showMobile();" class="mobile">
         <img class="mobile burger" src="{{asset('svg/burger.svg')}}" alt="">
@@ -18,17 +25,14 @@
     <a href="{{ route('contact')}}">contact</a>
     <a href="{{ route('shop')}}">@lang('header.shop')</a>
     <a href="{{ route('infos')}}">infos</a>
+    <a id="lang" class="{{ Route::is('portfolio') ? 'blue' : '' }}{{ Route::is('contact') ? 'brown' : '' }}" href="{{ App::getLocale() == 'fr' ? route('lang', 'en') : route('lang', 'fr')}}">
+    @if (App::getLocale() != 'fr')
+    <img src="{{ asset('img/FR.png')}}" alt="Download button">
+    @else
+    <img src="{{ asset('img/EN.png')}}" alt="Download button">
+    @endif
+</a>
     <button onclick="showMobile();" class="mobile">
         <img class="mobile close" src="{{asset('svg/close.svg')}}" alt="">
     </button>
 </nav>
-
-<a id="lang" class="{{ Route::is('portfolio') ? 'blue' : '' }}{{ Route::is('contact') ? 'brown' : '' }}" href="{{ App::getLocale() == 'fr' ? route('lang', 'en') : route('lang', 'fr')}}">
-    @if (App::getLocale() != 'fr')
-    <img src="{{ asset('img/FR.png')}}" alt="Download button">
-    <span>traduire</span>
-    @else
-    <img src="{{ asset('img/EN.png')}}" alt="Download button">
-    <span>translate</span>
-    @endif
-</a>
